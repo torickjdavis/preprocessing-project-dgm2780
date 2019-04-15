108e167b3a4ddf7921d0b00deda961bd2596a642
Torick Davis <torickjdavis@gmail.com>
2019-04-15 01:01:48 +0000

    base animations

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,10 @@
 "use strict";
+/* global TweenMax, TimelineMax, Sine */
 document.addEventListener("DOMContentLoaded", () => {
   let shopImages = document.querySelectorAll(".item-image");
-  for(let image of shopImages) {
+  for (let image of shopImages) {
     let sibling = image.previousElementSibling;
-    if(sibling != null && sibling.classList.contains("item-image-alt")) {
+    if (sibling != null && sibling.classList.contains("item-image-alt")) {
       image.addEventListener("mouseover", () => {
         image.classList.add("hovered");
         sibling.classList.remove("hovered");
@@ -25,4 +26,30 @@ document.addEventListener("DOMContentLoaded", () => {
 
     nav.classList.toggle("active");
   });
+
+  animatePage();
 });
+
+function animatePage() {
+  let tl = new TimelineMax();
+  tl.from('.logo .logo-name', 1, { opacity: 0 });
+  tl.from('.column.background-image', 2, { opacity: 0 });
+
+  TweenMax.staggerFrom('.item', 2, { opacity: 0, scale: 0.75 }, 0.5);
+
+  document.querySelectorAll('.item').forEach(item => {
+    let animation = new TimelineMax();
+    animation.pause(); // don't auto play
+    animation.to(item, 2, { scale: 0.8 });
+    animation.to(item, 2, { scale: 1 });
+    item.addEventListener('click', () => {
+      animation.restart();
+    });
+  });
+
+  document.querySelectorAll('.member-info').forEach(item => {
+    item.addEventListener('mousemove', () => {
+      TweenMax.to(item, 1, { borderRadius: "2rem" });
+    });
+  });
+}
